ce5dc16a2337c0c39c604f942563b8b134507192
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-27 01:56:44 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,8 +1,6 @@
 # Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
 
 PORTION_1 = {
   label: "Kingdom",
@@ -29,18 +27,7 @@ PORTION_4 = {
   label: "Species",
   sub_category: nil
 }
-# expect(starting_node[:sub_category][:sub_category][:sub_category][:label]).to eq("Order")
 def naming_system
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
   starting_node = 
   
   {
